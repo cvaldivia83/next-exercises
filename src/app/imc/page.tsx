24f6ee7e541b1d4de42400614692f1052ec78903
Carla Valdivia <valdivia.carla@gmail.com>
2025-10-27 00:08:52 -0300

    feat(page): add imc page

---
+++ src/app/imc/page.tsx
@@ -0,0 +1,12 @@
+import Container from "../components/Container";
+import H1Title from "../components/H1Title";
+
+const Imc = () => {
+  return (
+    <Container>
+      <H1Title>Calculadora de IMC</H1Title>
+    </Container>
+  )
+}
+
+export default Imc;
