a63f670c38663b68516542c78321e8005a912f47
Carla Valdivia <valdivia.carla@gmail.com>
2025-10-27 00:13:44 -0300

    fix(roboto): removed weights from roboto import and added display: swap to improve performance on client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Roboto } from 'next/font/google';
 import "./globals.css";
 import Provider from "./components/ThemeProvider";
 
-const roboto = Roboto({weight: ["100", "400", "500", "700", "900"], subsets: ["latin"]});
+const roboto = Roboto({weight: ["400", "700"], subsets: ["latin"], display: "swap"});
 
 export const metadata: Metadata = {
   title: "Aulas Origamid",
